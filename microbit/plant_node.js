248f982502ffd2dc94bb7aea1d72da1d6e060b67
Nur Ramadhan <nurramadhanjumali@yahoo.com>
2023-04-04 03:18:59 +0800

    Added Grove Speaker + Light Sensor.

--- microbit/plant_node.js
+++ microbit/plant_node.js
@@ -4,6 +4,10 @@ let BUGGY_MAX_DISTANCE = 80;
 let buggy_on_move = 1;
 let buggy_is_here = 0;
 
+/**
+ * SENSORS
+ */
+
 // Ultra-sonic reading frequency
 let us_reading_interval_onMove = 100;
 let us_reading_baselineTime = 0;
@@ -13,10 +17,17 @@ let us_cooldown_length = 30000; // 30 seconds
 let us_cooldown_baselineTime = 0;
 let us_cooldown = 0;
 
-/// Soil Moisture (sm) reading
+let buf_us: string[] = [];
+
+// Soil Moisture (sm) reading
 let sm_reading = 0;
 
-let buf_us: string[] = [];
+// Light Sensor reading
+let light_reading = 0;
+
+/**
+ * END
+ */
 
 radio.setGroup(8);
 radio.setTransmitSerialNumber(true);
@@ -35,22 +46,14 @@ basic.forever(function () {
   // soil moisture reading
   f_soil_moisture();
 
+  // light sensor reading
+  f_light_sensor();
+
   // Other readings <--------- Dan
 });
 
 /**
- * SOIL MOISTURE
- */
-function f_soil_moisture() {
-  sm_reading = pins.analogReadPin(AnalogPin.P0);
-  led.plotBarGraph(sm_reading, 1023);
-  if (input.buttonIsPressed(Button.A)) {
-    basic.showString("" + sm_reading);
-  }
-}
-
-/**
- * ULTRA SONIC
+ * ULTRASONIC RANGER SENSOR (PIN 0)
  */
 function f_ultrasonic() {
   if (buggy_on_move == 1 && us_cooldown == 0) {
@@ -78,10 +81,57 @@ function measure_distance() {
     buggy_on_move = 0;
     basic.showString("A");
   }
+
+  // Beep a warning tone when US detects an object within 5cm
+  f_checkForObstruction(distance)
 }
+
 /**
- * ENDS
+ * Grove Speaker (PIN 1)
+ * (Micro:bit will beep a warning tone and display a sad face when the ultrasonic ranger detects an object within 5 cm.)
  */
+function f_checkForObstruction(distance) {
+  if (distance <= 5) {
+    basic.showIcon(IconNames.Sad);
+    music.playTone(988, music.beat(BeatFraction.Whole));
+  } else {
+    basic.showIcon(IconNames.Happy);
+  }
+}
+
+/**
+ * LIGHT SENSOR (PIN 2)
+ * (Output is from 0 – 630 (maximum brightness))
+ */
+function f_light_sensor() {
+  light_reading = pins.analogReadPin(AnalogPin.P2);
+
+  if (light_reading < 210) {
+    // Dark
+    basic.showNumber(light_reading);
+  } else {
+    // Bright (>= 210)
+
+    basic.showNumber(light_reading);
+  }
+
+  //Testing
+  led.plotBarGraph(light_reading, 630);
+}
+
+/**
+ * SOIL MOISTURE SENSOR (PIN 8)
+ */
+function f_soil_moisture() {
+  sm_reading = pins.analogReadPin(AnalogPin.P8);
+
+  if (input.buttonIsPressed(Button.A)) {
+    basic.showString("" + sm_reading);
+  }
+
+  //Testing
+  led.plotBarGraph(sm_reading, 1023);
+}
 
 radio.onReceivedString(function (receivedString) {
   // Communication between MicroBits (notify)
