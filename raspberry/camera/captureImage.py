ffa25edb98e327dfa3d93bd9892033e2c045bc25
Rashini <it18113150@my.sliit.lk>
2023-04-08 17:08:51 +0800

    captureImage.py

--- raspberry/camera/captureImage.py
+++ raspberry/camera/captureImage.py
@@ -1,15 +1,45 @@
 import time
+import sqlite3
+import serial
+from predict_disease import predict
 from picamera2 import Picamera2, Preview
 
-picam = Picamera2()
+PORT = "/dev/ttyACM0"
+##
+BAUD = 115200
+s = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=1)
 
-config = picam.create_preview_configuration()
-picam.configure(config)
 
-picam.start_preview(Preview.QTGL)
+def captureAndpredict():
+	picam = Picamera2()
 
-picam.start()
-time.sleep(2)
-picam.capture_file("leaf.jpg")
+	config = picam.create_preview_configuration()
+	picam.configure(config)
 
-picam.close()
+	picam.start_preview(Preview.QTGL)
+
+	picam.start()
+	time.sleep(2)
+	picam.capture_file("leaf.jpg")
+
+	picam.close()
+
+	disease = predict()
+	
+while True:
+    
+    #message = s.readline().decode().strip()
+    
+    message = s.readline()
+    message = message.decode('utf-8').strip()
+    #print(s.readline().decode('utf-8'))
+    print("message:", message)
+    if message == 'predict':
+		
+        # Call the on_microbit_message method with the received message
+        captureAndpredict()
+        response = 'move'
+        s.write(str.encode(response))
+        #s.write(response.encode('utf-8'))
+
+s.close()
