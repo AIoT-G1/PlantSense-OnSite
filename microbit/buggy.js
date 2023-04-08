e21ae1fbcd0da71f412d9d0bd85e3b71349623b5
xueqi <72065687+dengxueqi@users.noreply.github.com>
2023-04-08 16:07:22 +0800

    update buggy.js

--- microbit/buggy.js
+++ microbit/buggy.js
@@ -1,80 +1,81 @@
 // Initialize UART let uart = serial;
 // uart.setup(115200);
 radio.onReceivedString(function (receivedString) {
-    // Receive signals from other MB
-    if (receivedString.includes("notify=")) {
-        buf_us = receivedString.split("=");
-        // Receive arrival signal
-        if (buf_us[1] == "arrival") {
-            stop_buggy()
-            take_pictures(radio.receivedPacket(RadioPacketProperty.SerialNumber))
-        }
+  // Receive signals from other MB
+  if (receivedString.includes("notify=")) {
+    buf_us = receivedString.split("=");
+    // Receive arrival signal
+    if (buf_us[1] == "arrival") {
+      stop_buggy();
+      take_pictures(radio.receivedPacket(RadioPacketProperty.SerialNumber));
+      continue_driving = true;
     }
-})
+  }
+});
 // serial.redirectToUSB()
 function stop_buggy() {
-    basic.showString("S")
-    continue_driving = false
-    basic.pause(300)
+  basic.showString("S");
+  continue_driving = false;
+  basic.pause(300);
 }
 // Code for disease detection <--------- Rashini
 function take_pictures(curr_plant_dn: number) {
-    // serial.writeLine("cmd=take_picture:" +
-    // curr_plant_dn)
-    basic.showString("P")
-    basic.pause(300)
-    basic.showNumber(curr_plant_dn)
-    serial.writeString("predict")
-    serial_response = serial.readLine()
-    if (serial_response == "move") {
-        continue_driving = true
-        // basic.uart.writeString('predict')
-        radio.sendString("notify=departure")
-    }
+  // serial.writeLine("cmd=take_picture:" +
+  // curr_plant_dn)
+  basic.showString("P");
+  basic.pause(300);
+  //basic.showNumber(curr_plant_dn)
+  serial.writeString("predict");
+  serial_response = serial.readLine();
+  if (serial_response == "move") {
+    continue_driving = true;
+    // basic.uart.writeString('predict')
+    radio.sendString("notify=departure");
+  }
 }
 function turnLeft(degrees: number) {
-    timeToWait = degrees * MICROSEC_IN_A_SEC / DEGREES_PER_SEC
-    pins.servoWritePin(AnalogPin.P1, 45)
-    pins.servoWritePin(AnalogPin.P2, 45)
-    control.waitMicros(timeToWait)
-    pins.servoWritePin(AnalogPin.P1, 90)
-    pins.servoWritePin(AnalogPin.P2, 90)
+  timeToWait = (degrees * MICROSEC_IN_A_SEC) / DEGREES_PER_SEC;
+  pins.servoWritePin(AnalogPin.P1, 45);
+  pins.servoWritePin(AnalogPin.P2, 45);
+  control.waitMicros(timeToWait);
+  pins.servoWritePin(AnalogPin.P1, 90);
+  pins.servoWritePin(AnalogPin.P2, 90);
 }
 function driveForward(distance: number) {
-    timeToWait2 = distance * MICROSEC_IN_A_SEC / DISTANCE_PER_SEC
-    pins.servoWritePin(AnalogPin.P1, 0)
-    pins.servoWritePin(AnalogPin.P2, 180)
-    control.waitMicros(timeToWait2)
-    pins.servoWritePin(AnalogPin.P1, 90)
-    pins.servoWritePin(AnalogPin.P2, 90)
+  timeToWait2 = (distance * MICROSEC_IN_A_SEC) / DISTANCE_PER_SEC;
+  pins.servoWritePin(AnalogPin.P1, 0);
+  pins.servoWritePin(AnalogPin.P2, 180);
+  control.waitMicros(timeToWait2);
+  pins.servoWritePin(AnalogPin.P1, 90);
+  pins.servoWritePin(AnalogPin.P2, 90);
 }
-let timeToWait2 = 0
-let timeToWait = 0
-let serial_response = ""
-let DEGREES_PER_SEC = 0
-let DISTANCE_PER_SEC = 0
-let MICROSEC_IN_A_SEC = 0
-let continue_driving = false
-let buf_us: string[] = []
-MICROSEC_IN_A_SEC = 1000000
-DISTANCE_PER_SEC = 100
-DEGREES_PER_SEC = 165
-let radioChannel = 7
-let uartBaudRate = 115200
-radio.setGroup(8)
-radio.setTransmitSerialNumber(true)
-radio.setTransmitPower(8)
-serial.redirectToUSB()
+let timeToWait2 = 0;
+let timeToWait = 0;
+let serial_response = "";
+let DEGREES_PER_SEC = 0;
+let DISTANCE_PER_SEC = 0;
+let MICROSEC_IN_A_SEC = 0;
+let continue_driving = false;
+let buf_us: string[] = [];
+MICROSEC_IN_A_SEC = 1000000;
+DISTANCE_PER_SEC = 100;
+DEGREES_PER_SEC = 165;
+let radioChannel = 7;
+let uartBaudRate = 115200;
+radio.setGroup(8);
+radio.setTransmitSerialNumber(true);
+radio.setTransmitPower(8);
+serial.redirectToUSB();
 basic.forever(function () {
-    input.onButtonPressed(Button.A, function () {
-        continue_driving = true;
-        while (continue_driving) {
-            basic.pause(500); //pauses buggy to move hand
-            driveForward(350);
-            turnLeft(90);
-        }
-    });
-    input.onButtonPressed(Button.B, function () {
-        continue_driving = false;
-    });
-})
+  input.onButtonPressed(Button.A, function () {
+    continue_driving = true;
+    while (continue_driving) {
+      basic.pause(500); //pauses buggy to move hand
+      driveForward(350);
+      turnLeft(90);
+    }
+  });
+  input.onButtonPressed(Button.B, function () {
+    continue_driving = false;
+  });
+});
